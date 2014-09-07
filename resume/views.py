d2c8aadfae2a6b63d6fd81b54e582e40ea4a42a3
Garet McKinley <garetmckinley@me.com>
2014-09-06 21:14:11 -0400

    PEP8 compliance fixes and set template to .jade

--- resume/views.py
+++ resume/views.py
@@ -3,6 +3,7 @@ from resume.models import Bio
 
 # Create your views here.
 
+
 def index(request):
-	bio = Bio.objects.filter()
-	return render(request, 'resume/index.html', {'bio':bio})
+    bio = Bio.objects.filter()
+    return render(request, 'index.jade', {'bio': bio})
